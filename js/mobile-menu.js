cbd54fcfaa16d4aea7574f0a47930943edd32290
nikevs <nitkevs@gmail.com>
2020-08-25 16:36:19 +0300

    Исправил глюк анимации мобильного меню.
    Иногда, если закрыть меню и быстро его открыть, первая анимация,
    видимо, не успевала пройти полностью, из-за чего вторая
    происходила с ошибкой.

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,23 +3,25 @@ let menu = document.getElementById('sidebar-first');
 let openMenuButton = document.getElementById('open-menu-button');
 let closeMenuButton = document.getElementById('close-menu-button');
 openMenuButton.addEventListener('click', function() {
-    menu.style.opacity = "1";
-    menu.style.zIndex = "3";
+    menu.style.opacity = '1';
+    menu.style.zIndex = '3';
     items = document.querySelectorAll('#sidebar-first ul.menu a, #sidebar-first .field-content a, #sidebar-first .h2-container h2, #sidebar-first .tagclouds-term a');
   for (i = 0; i < items.length; i++) {
-    items[i].style.transitionDelay = i/50  + "s";
-    items[i].style.transform = "translateX(0%)";
+    items[i].style.transitionProperty = 'transform';
+    items[i].style.transitionDelay = i/50  + 's';
+    items[i].style.transform = 'translateX(0%)';
   }
 });
 
 closeMenuButton.addEventListener('click', function() {
-   menu.style.opacity = "0";
+   menu.style.opacity = '0';
    setTimeout(function() {
      menu.style.zIndex = '0';
      for (i = 0; i < items.length; i++) {
-     items[i].style.transform = "translateX(-100%)";
+     items[i].style.transitionProperty = 'none';
+     items[i].style.transform = 'translateX(-100%)';
      }
-   }, 600);
+   }, 300);
 });
 
 
